0cfbe998dface0e92fd84893ba7ed865e9b92f3c
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-08 11:30:11 -0700

    created custom user model

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +86,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
