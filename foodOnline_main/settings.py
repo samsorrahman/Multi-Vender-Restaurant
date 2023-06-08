6c1541fa48f7a792a501ce6c66dcdabce073e4b2
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-08 13:32:37 -0700

    configration medai done

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -127,6 +127,10 @@ STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
