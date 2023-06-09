bbfccb6044dc9717477444fa745b3e5e24bccb83
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-09 00:16:31 -0700

    user creating 2 method to hash password

--- accounts/views.py
+++ accounts/views.py
@@ -10,10 +10,23 @@ def registerUser(request):
     if request.method == 'POST':
         form= UserForm(request.POST)
         if form.is_valid():
-            user= form.save(commit=False)
-            user.role=User.CUSTOMER
+            # we can hash password in 2 ways 1st is here
+            # password=form.cleaned_data['password']
+            # user= form.save(commit=False)
+            # user.set_password(password)
+            # user.role=User.CUSTOMER
+            # user.save()
+            # return redirect('home')
+
+            # Create the user using create_user method 2nd method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
             user.save()
-            return redirect('home')
     else:
         form= UserForm()
     context={
