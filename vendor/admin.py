9c99a9061fc0f4242e82a3b29aafc237f97a070f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-09 04:57:05 -0700

    vendor admin config

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Vendor
-
-# Register your models here.
+from vendor.models import Vendor
 
 
-admin.site.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
 
+
+# class OpeningHourAdmin(admin.ModelAdmin):
+#     list_display = ('vendor', 'day', 'from_hour', 'to_hour')
+
+admin.site.register(Vendor, VendorAdmin)
+# admin.site.register(OpeningHour, OpeningHourAdmin)
