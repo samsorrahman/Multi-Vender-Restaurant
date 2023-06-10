aca43b1fbf153e07a3a699a4780295a0e59d78e5
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-09 23:32:21 -0700

    restrict user to access unauthorized pages

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,27 @@ from .forms import UserForm
 from django.http import HttpResponse
 from .models import User, UserProfile
 from django.contrib import messages, auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 # Create your views here.
+
+
+
+
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
 
 
 def registerUser(request):
@@ -118,9 +137,14 @@ def myAccount(request):
     return redirect(redirectUrl)
 
 @login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
+
+
+
 @login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/VendorDashboard.html')
