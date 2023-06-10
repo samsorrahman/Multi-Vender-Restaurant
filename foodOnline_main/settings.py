cebfe9fa1ed1cfd5741a51a70e4b338f53f94fef
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-09 23:41:26 -0700

    Email Configurations

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -142,3 +142,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Configuartion
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rahmansamsor82@gmail.com'
+EMAIL_HOST_PASSWORD = 'ahquzaspwlvrcndd'
