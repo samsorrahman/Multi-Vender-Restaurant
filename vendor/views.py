abd14fc15e9a44c72fef99294e7f8d7e39d3e1ed
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-11 02:49:01 -0700

    store vendor profile

--- vendor/views.py
+++ vendor/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from accounts.forms import UserProfileForm
 from .forms import VendorForm
 # Create your views here.
 from accounts.models import UserProfile
 from .models import Vendor
+from django.contrib import messages
 
 def myprofile(request):
     # auto filled data
     profile= get_object_or_404(UserProfile, user=request.user)
     vendor= get_object_or_404(Vendor, user=request.user)
-    profile_form = UserProfileForm(instance=profile)
-    vendor_form = VendorForm(instance=vendor)
     
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form= VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Settings Updated')
+            return redirect('myprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
+        
     context ={
         'profile_form': profile_form,
         'vendor_form': vendor_form,
