975c26adf2d9821c0c8aef17ba3c31471f7a0b65
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-12 03:52:26 -0700

    Menu Builder Fooditem frontend

--- vendor/urls.py
+++ vendor/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', accountViews.vendorDashboard, name='vendor'),
     path('profile/', views.myprofile, name='myprofile'),
     path('menu_builder/', views.menu_builder, name="menu-builder"),
+    path('menu_builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
 ]
