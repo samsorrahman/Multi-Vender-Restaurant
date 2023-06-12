c197079ab558e548d8e889adaa6e493c42e4b664
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-11 22:41:43 -0700

    getting the latitude and longtitude from google of an address

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -21,6 +21,31 @@ function onPlaceChanged (){
     }
     else{
         console.log('place name=>', place.name)
+        // console.log('Samsor Rahman is shining')
     }
-    // get the address components and assign them to the fields
+
+
+// get the address components and assign them to the fields
+    // console.log(place);
+    var geocoder = new google.maps.Geocoder()
+    var address = document.getElementById('id_address').value
+
+    geocoder.geocode({'address': address}, function(results, status){
+        // console.log('results=>', results)
+        // console.log('status=>', status)
+        if(status == google.maps.GeocoderStatus.OK){
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+
+            // console.log('lat=>', latitude);
+            // console.log('long=>', longitude);
+            $('#id_latitude').val(latitude);
+            $('#id_longitude').val(longitude);
+
+            $('#id_address').val(address);
+        }
+    });
+
+    // loop through the address components and assign other address data
+
 }
