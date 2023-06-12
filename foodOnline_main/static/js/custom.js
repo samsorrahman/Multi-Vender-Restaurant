2f0363ed89c3c341e373286f7de8f289e0b6074f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-11 23:47:11 -0700

    loop through address components and fetch data rendering other fields from google apis

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -6,7 +6,7 @@ autocomplete = new google.maps.places.Autocomplete(
     {
         types: ['geocode', 'establishment'],
         //default in this app is "IN" - add your country code
-        componentRestrictions: {'country': ['in']},
+        componentRestrictions: {'country': ['AFG', 'PK']},
     })
 // function to specify what should happen when the prediction is clicked
 autocomplete.addListener('place_changed', onPlaceChanged);
@@ -47,5 +47,32 @@ function onPlaceChanged (){
     });
 
     // loop through the address components and assign other address data
+    console.log(place.address_components);
+    for(var i=0; i<place.address_components.length; i++){
+        for(var j=0; j<place.address_components[i].types.length; j++){
+                 // get country
+                 if(place.address_components[i].types[j] == 'country'){
+                    $('#id_country').val(place.address_components[i].long_name);
+                }
+
+                     // get state
+            if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+                $('#id_state').val(place.address_components[i].long_name);
+            }
+
+             // get city
+             if(place.address_components[i].types[j] == 'locality'){
+                $('#id_city').val(place.address_components[i].long_name);
+            }
+
+              // get pincode
+              if(place.address_components[i].types[j] == 'postal_code'){
+                $('#id_pin_code').val(place.address_components[i].long_name);
+            }else{
+                $('#id_pin_code').val("");
+            }
+
+        }
+    }
 
 }
