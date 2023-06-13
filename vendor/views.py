339d65d91cc5ab3054f760099a1e5d2d4516a52e
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-13 03:44:22 -0700

    Edit Food Category for loggedin Vendors

--- vendor/views.py
+++ vendor/views.py
@@ -187,7 +187,7 @@ def edit_food(request, pk=None):
 
     else:
         form = FoodItemForm(instance=food)
-        # form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form': form,
         'food': food,
