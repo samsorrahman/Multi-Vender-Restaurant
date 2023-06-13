261e0ee8ef99fceb1be745ba2e766fab4cb7e1da
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-13 01:28:46 -0700

    Edit Food Item CRUD

--- vendor/urls.py
+++ vendor/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     
     # Food Item CRUD
     path('menu_builder/food/add/', views.add_food, name='add_food'),
-    # path('menu_builder/food/edit/<int:pk>/', views.edit_food, name='edit_food'),
-    # path('menu_builder/food/delete/<int:pk>/', views.delete_food, name='delete_food'),
+    path('menu_builder/food/edit/<int:pk>/', views.edit_food, name='edit_food'),
+    path('menu_builder/food/delete/<int:pk>/', views.delete_food, name='delete_food'),
 
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -162,5 +162,38 @@ def add_food(request):
         # form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form': form,
+        'food': food,
     }
     return render(request, 'vendor/add_food.html', context)
+
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def edit_food(request, pk=None):
+    food = get_object_or_404(FoodItem, pk=pk)
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES, instance=food)
+        if form.is_valid():
+            foodtitle = form.cleaned_data['food_title']
+            food = form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(foodtitle)
+            form.save()
+            messages.success(request, 'Food Item updated successfully!')
+            return redirect('fooditems_by_category', food.category.id)
+        else:
+            print(form.errors)
+
+    else:
+        form = FoodItemForm(instance=food)
+        # form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    context = {
+        'form': form,
+        'food': food,
+    }
+    return render(request, 'vendor/edit_food.html', context)
+
+
+def delete_food(request):
+    pass
