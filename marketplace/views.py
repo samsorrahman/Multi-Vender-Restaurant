45013991dcd71e38c05a4a19bb11e87613637694
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-14 03:39:40 -0700

    Display fooditems by category using prefetch related

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-
+from menu.models import Category, FoodItem
 from vendor.models import Vendor
+from django.db.models import Prefetch
 # Create your views here.
 
 def marketplace(request):
@@ -15,7 +16,14 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset = FoodItem.objects.filter(is_available=True)
+        )
+    )
     context = {
        'vendor': vendor,
+       'categories': categories,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
