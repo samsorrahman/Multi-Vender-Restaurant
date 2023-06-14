a27b8cf82f3f20ea98d5aa40537c4df4eb25b56b
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-14 12:04:29 -0700

    update the cart counter and item qty Realtime

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -94,7 +94,8 @@ $(document).ready(function(){
             url: url,
             data:data,
             success: function(response){
-                console.log(response)
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                $('#qty-'+food_id).html(response.qty);
             }
         })
     })
--- marketplace/views.py
+++ marketplace/views.py
@@ -4,6 +4,7 @@ from menu.models import Category, FoodItem
 from vendor.models import Vendor
 from django.db.models import Prefetch
 from .models import Cart
+from .context_processors import get_cart_counter
 # Create your views here.
 
 def marketplace(request):
@@ -50,10 +51,10 @@ def add_to_cart(request, food_id):
                     # Increase the cart quantity
                     chkCart.quantity += 1
                     chkCart.save()
-                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity'})
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
                 except:
                     chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart'})
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart',  'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
             except:
                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
         else:
