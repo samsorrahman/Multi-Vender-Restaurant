b44848499cd3ba0d3aeb29a97a619591026a2a0f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-14 04:13:03 -0700

    create cart model

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fooditem', 'quantity', 'updated_at')
+
+
+# class TaxAdmin(admin.ModelAdmin):
+#     list_display = ('tax_type', 'tax_percentage', 'is_active')
+
+
+admin.site.register(Cart, CartAdmin)
+# admin.site.register(Tax, TaxAdmin)
