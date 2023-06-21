1fb736630e30383ddb47debc2b5a99a5ad4daf64
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-21 15:33:03 -0700

    update customer Profile

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
+from django.contrib import messages
 # Create your views here.
 
 
@@ -10,11 +11,24 @@ from accounts.models import UserProfile
 def cprofile(request):
     
     profile = get_object_or_404(UserProfile, user=request.user)
-    profile_form = UserProfileForm(instance=profile)
-    user_form = UserInfoForm(instance=request.user)
+    
+    if request.method=='POST':
+        profile_form = UserProfileForm(request.POST, request.FILES,instance=profile)
+        user_form = UserInfoForm(request.POST,instance=request.user)
+        
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, 'Profile Updated')
+            return redirect('cprofile')
+                    
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
 
     context = {
         'profile_form': profile_form,
         'user_form' : user_form,
+        'profile': profile,
     }
     return render(request, 'customers/cprofile.html', context)
