70ea54ea916dccf3ca613cbdae0ecb4d31f73e4d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-21 03:45:02 -0700

    update tax in the fornted

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -98,7 +98,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
                 }
@@ -140,7 +140,7 @@ $(document).ready(function(){
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -210,17 +210,16 @@ $(document).ready(function(){
     function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
 
-            // console.log(tax_dict)
-            // for(key1 in tax_dict){
-            //     console.log(tax_dict[key1])
-            //     for(key2 in tax_dict[key1]){
-            //         // console.log(tax_dict[key1][key2])
-            //         $('#tax-'+key1).html(tax_dict[key1][key2])
-            //     }
-            // }
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
