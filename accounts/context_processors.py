d70ff581ee8c5f798674d22cbe841ef6c327a8ce
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-21 15:52:33 -0700

    load cover and profile pics

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -12,12 +12,12 @@ def get_vendor(request):
     return dict(vendor=vendor)
 
 
-# def get_user_profile(request):
-#     try:
-#         user_profile = UserProfile.objects.get(user=request.user)
-#     except:
-#         user_profile = None
-#     return dict(user_profile=user_profile)
+def get_user_profile(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except:
+        user_profile = None
+    return dict(user_profile=user_profile)
 
 
 
