520cedecc08c51d86c2e2e60a2c37b3db943a29b
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-22 09:53:54 -0700

    prepopulated billing form

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
+from accounts.models import UserProfile
 from menu.models import Category, FoodItem
 from orders.forms import OrderForm
 from vendor.models import OpeningHour, Vendor
@@ -169,17 +170,29 @@ def search(request):
 
 
 
+
+@login_required(login_url='login')
 def checkout(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
-    cart_count= cart_items.count()
-    
+    cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('marketplace')
-    form= OrderForm()
     
-    context={
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'phone': request.user.phone_number,
+        'email': request.user.email,
+        'address': user_profile.address,
+        'country': user_profile.country,
+        'state': user_profile.state,
+        'city': user_profile.city,
+        'pin_code': user_profile.pin_code,
+    }
+    form = OrderForm(initial=default_values)
+    context = {
         'form': form,
-        'cart_count': cart_count,
-        'cart_items': cart_items
+        'cart_items': cart_items,
     }
     return render(request, 'marketplace/checkout.html', context)
