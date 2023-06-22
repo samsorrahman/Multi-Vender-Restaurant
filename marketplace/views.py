78bbcce9e8a0634ee8c9f1665e03d244df85ac1e
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-22 09:31:43 -0700

    list cart item in checkout page

--- marketplace/views.py
+++ marketplace/views.py
@@ -170,9 +170,16 @@ def search(request):
 
 
 def checkout(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    cart_count= cart_items.count()
+    
+    if cart_count <= 0:
+        return redirect('marketplace')
     form= OrderForm()
     
     context={
-        'form': form
+        'form': form,
+        'cart_count': cart_count,
+        'cart_items': cart_items
     }
     return render(request, 'marketplace/checkout.html', context)
